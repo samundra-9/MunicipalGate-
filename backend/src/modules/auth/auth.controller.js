5209c9acaaeb6aa9de83b31324d6984224fd65a6
Samundra Poudel <samundrapoudel@Samundras-MacBook-Air.local>
2026-03-18 14:46:07 +0530

    added manual user registration

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,11 +1,20 @@
+// backend/src/modules/auth/auth.controller.js
 import { authService } from "./auth.service.js";
 
-export async function login(req, res, next) {
-    // console.log("LOGIN ROUTE HIT");
-
+export async function register(req, res, next) {
   try {
-    const token = await authService.login(req.body);
-    res.json({ token });
+    const { name, email, password, municipalityId } = req.body;
+    const user = await authService.register({ 
+      name, 
+      email, 
+      password,
+      municipalityId: municipalityId ? Number(municipalityId) : null 
+    });
+    res.status(201).json({ 
+      id: user.id, 
+      email: user.email, 
+      role: user.role.name 
+    });
   } catch (error) {
     next(error);
   }
